1e99217e1c8bec9a7161394dfa5778e7b5920bcd
jupegarnica <jupegarnica@gmail.com>
2019-05-13 10:57:04 +0200

    logic for more films (pagination) done

--- src/app/api-movies.service.ts
+++ src/app/api-movies.service.ts
@@ -2,23 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class ApiMoviesService {
   urlBase = `https://api.themoviedb.org/3/`;
   key = 'da63f96158928b6fc3e74f1d20e6a5ce';
-  generateUrl = (type: string) => this.urlBase + `movie/${type}?api_key=` + this.key;
+  // generateUrl = (type: string) => this.urlBase + `movie/${type}?api_key=` + this.key;
+  generateUrl = (type: string, page = 1) => `${this.urlBase}movie/${type}?api_key=${this.key}&page=${page}`;
+
   generateSearchUrl = (query: string) => `https://api.themoviedb.org/3/search/movie?api_key=${this.key}&query=${query}`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getMovies(type: string) {
-    return this.http.get(this.generateUrl(type));
+  getMovies(type: string, page: number) {
+    return this.http.get(this.generateUrl(type, page));
   }
 
   getMovie(id: string) {
-    return this.http.get(this.generateUrl(id))
+    return this.http.get(this.generateUrl(id));
   }
 
   getSimilarMovies(id: string) {
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -6,31 +6,32 @@ import { ApiMoviesService } from '../api-movies.service';
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.scss']
+  styleUrls: ['./movie-list.component.scss'],
 })
 export class MovieListComponent implements OnInit {
   type: string;
   typeSubscription: any;
-  movies: object[];
+  movies: object[] = [];
+  lastPagedLoaded = 0;
   validTypes = ['top_rated', 'popular', 'upcoming'];
 
-  constructor(
-    private route: ActivatedRoute,
-    private api: ApiMoviesService,
-    private router: Router
-  ) { }
+  constructor(private route: ActivatedRoute, private api: ApiMoviesService, private router: Router) {}
 
   ngOnInit() {
     this.typeSubscription = this.route.params.subscribe(params => {
       this.type = params.type;
       if (this.validTypes.includes(params.type)) {
-        this.api.getMovies(this.type).subscribe((res: any) => {
-          this.movies = res.results;
-        })
+        this.moreMovies();
       } else {
         this.router.navigate(['/movies/popular']);
       }
-    })
+    });
+  }
+  moreMovies() {
+    this.api.getMovies(this.type, this.lastPagedLoaded + 1).subscribe((res: any) => {
+          this.movies = [...this.movies, ...res.results];
+          this.lastPagedLoaded = res.page;
+    });
   }
 
   ngOnDestroy() {
